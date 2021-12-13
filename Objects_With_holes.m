5e43d637f510af087a9022d8c50a1beaf8c2038a
Esraa Hosny <47479912+Esraahosny25@users.noreply.github.com>
2021-12-14 00:51:20 +0200

    Update Objects_With_holes.m

--- Objects_With_holes.m
+++ Objects_With_holes.m
@@ -1,10 +1,12 @@
-function [owh_count] = Objects_With_holes(Image)
+function [cols] = Objects_With_holes(Image)
     %se = strel('square',10);
     %im = imopen(Image,se);
     %figure,imshow(im);
     %[ label, owh_count ] = bwlabel(im);
 
     owh_im = regionprops(Image, 'EulerNumber');
-    owh_count = find([STATS.EulerNumber] <= 0);
-
+    owh_count = find([ owh_im.EulerNumber] <= 0);
+    [rows cols]=size(owh_count);
+   
+    
 end
